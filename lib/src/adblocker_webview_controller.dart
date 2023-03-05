c015a9bf8437f1bd70a0e8a2317aa415909f89b0
Md Didarul islam <islamdidarmd@gmail.com>
2023-03-05 21:01:05 +0600

    Fixed dart file format

--- lib/src/adblocker_webview_controller.dart
+++ lib/src/adblocker_webview_controller.dart
@@ -2,6 +2,7 @@ import 'domain/use_case/fetch_banned_host_use_case.dart';
 import 'package:adblocker_webview/src/service_locator.dart';
 
 import 'domain/entity/host.dart';
+
 /// The controller for [AdBlockerWebviewWidget].
 /// Below is and Example of getting a singleton instance:
 /// ```dart
@@ -17,9 +18,15 @@ import 'domain/entity/host.dart';
 ///   }
 /// ```
 ///
+///
+
+///ignore_for_file: avoid-late-keyword
+///ignore_for_file: avoid-non-null-assertion
 class AdBlockerWebviewController {
   static AdBlockerWebviewController? _instance;
-  late final FetchBannedHostUseCase _fetchBannedHostUseCase; ///ignore: avoid-late-keyword
+
+  late final FetchBannedHostUseCase _fetchBannedHostUseCase;
+
   final _bannedHost = <Host>[];
 
   static AdBlockerWebviewController get instance {
@@ -27,13 +34,13 @@ class AdBlockerWebviewController {
       _instance = AdBlockerWebviewController._internal();
     }
 
-    return _instance!; ///ignore: avoid-non-null-assertion
+    return _instance!;
   }
 
   AdBlockerWebviewController._internal();
 
   Future<void> initialize() async {
-    final getIt = configureDependencies(); ///ignore: unused_local_variable
+    configureDependencies();
     _fetchBannedHostUseCase = ServiceLocator.get<FetchBannedHostUseCase>();
 
     final hosts = await _fetchBannedHostUseCase.execute();
