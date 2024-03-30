2c713cbcf2ff31e9b189650a93ecb7a0ab813ffe
Md Didarul islam <islamdidarmd@gmail.com>
2024-03-30 23:22:13 +0800

    Add caching api response for better performance

--- lib/src/data/repository/adblocker_repository_impl.dart
+++ lib/src/data/repository/adblocker_repository_impl.dart
@@ -3,17 +3,18 @@ import 'dart:convert';
 import 'package:adblocker_webview/src/domain/entity/host.dart';
 import 'package:adblocker_webview/src/domain/repository/adblocker_repository.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class AdBlockerRepositoryImpl implements AdBlockerRepository {
+  final _url =
+      'https://pgl.yoyo.org/as/serverlist.php?hostformat=nohtml&showintro=0';
+
   @override
   Future<List<Host>> fetchBannedHostList() async {
-    const url =
-        'https://pgl.yoyo.org/as/serverlist.php?hostformat=nohtml&showintro=0';
     try {
-      final response = await http.get(Uri.parse(url));
+      final response = await _getDataWithCache(_url);
 
-      return LineSplitter.split(response.body)
+      return LineSplitter.split(response)
           .map((e) => Host(authority: e))
           .toList();
     } catch (e) {
@@ -23,5 +24,11 @@ class AdBlockerRepositoryImpl implements AdBlockerRepository {
 
       return [];
     }
+  }
+
+  Future<String> _getDataWithCache(String url) async {
+    final cacheManager = DefaultCacheManager();
+    final file = await cacheManager.getSingleFile(url);
+    return file.readAsString();
   }
 }
