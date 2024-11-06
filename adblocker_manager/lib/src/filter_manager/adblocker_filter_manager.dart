bb2d8083b5268dc8654ebd017ac356aaa0eea332
Md Didarul islam <islamdidarmd@gmail.com>
2024-11-06 08:46:02 +0800

    Remove context dependency from manager

--- adblocker_manager/lib/src/filter_manager/adblocker_filter_manager.dart
+++ adblocker_manager/lib/src/filter_manager/adblocker_filter_manager.dart
@@ -5,17 +5,17 @@ import 'package:adblocker_manager/gen/assets.gen.dart';
 import 'package:adblocker_manager/src/config/adblocker_filter_config.dart';
 import 'package:adblocker_manager/src/config/filter_type.dart';
 import 'package:adblocker_manager/src/filter_manager/filter_manager.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
+
 
 class AdBlockerFilterManager implements FilterManager {
-  AdBlockerFilterManager(this.context);
+  AdBlockerFilterManager();
 
-  final BuildContext context;
   final List<AdBlockerFilter> _filters = [];
 
   @override
   Future<void> init(AdBlockerFilterConfig config) async {
-    await _createFilters(context, config.types);
+    await _createFilters(config.types);
   }
 
   @override
@@ -25,29 +25,27 @@ class AdBlockerFilterManager implements FilterManager {
     }
   }
 
-  Future<void> _createFilters(
-      BuildContext context, List<FilterType> types) async {
+  Future<void> _createFilters(List<FilterType> types) async {
     for (final type in types) {
-      final filter = await _filterfromType(context, type);
+      final filter = await _filterfromType(type);
       _filters.add(filter);
     }
   }
 
-  Future<AdBlockerFilter> _filterfromType(
-    BuildContext context,
-    FilterType type,
-  ) async {
+  Future<AdBlockerFilter> _filterfromType(FilterType type,) async {
     late String rawData;
+    const packagePrefix = 'packages/adblocker_manager/';
+
     switch (type) {
       case FilterType.adguardBase:
         rawData =
-            await DefaultAssetBundle.of(context).loadString(Assets.adguardBase);
+            await rootBundle.loadString('$packagePrefix${Assets.adguardBase}');
       case FilterType.adguardAnnyoance:
-        rawData = await DefaultAssetBundle.of(context)
-            .loadString(Assets.adguardAnnyoance);
+        rawData = await rootBundle
+            .loadString('$packagePrefix${Assets.adguardAnnyoance}');
       case FilterType.easyPrivacyLite:
-        rawData = await DefaultAssetBundle.of(context)
-            .loadString(Assets.easyPrivacyLite);
+        rawData = await rootBundle
+            .loadString('$packagePrefix${Assets.easyPrivacyLite}');
     }
 
     final filter = AdBlockerFilter();
