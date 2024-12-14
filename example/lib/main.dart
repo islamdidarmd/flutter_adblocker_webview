c753a62e0cec4d4772e06dc95ff9d435cb13f512
Md Didarul islam <islamdidarmd@gmail.com>
2024-12-14 21:12:37 +0800

    Replace InApp webview with webview flutter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Adblocker Webview',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Adblocker Webview'),
     );
   }
 }
